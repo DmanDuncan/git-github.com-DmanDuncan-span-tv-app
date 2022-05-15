91319131f1bbc01bc9517f73186350e1d9c3a120
DmanDuncan <duncanhendry101@gmail.com>
2022-05-15 08:34:10 +0200

    commenting code

--- src/components/home-image-container.js
+++ src/components/home-image-container.js
@@ -3,24 +3,25 @@ export function HomeImageContainer({homePhotosSetOne, homePhotosSetTwo}) {
     <div className={"image-container"}>
         <div className={"image-wrapper"} onWheel = {(e) => 
           { 
+            // scroll left and right on mouse wheel scroll
             const scrollContainer = document.querySelector(".image-wrapper");
-              scrollContainer.scrollLeft += e.deltaY;
+            scrollContainer.scrollLeft += e.deltaY;
           }}>
-
           {homePhotosSetOne.map(image => ( 
             <img className={"image-item"} src={image.urls.regular} key={image.id}/>
           ))}
-
           {homePhotosSetTwo.map(image => ( 
             <img className={"image-item"} src={image.urls.regular} key={image.id}/>
           ))}
           <p className={"button button-left"} onClick={(e) => 
           {
+              // scroll left on button click
               const scrollContainer = document.querySelector(".image-wrapper");
               scrollContainer.scrollLeft -= 250;
           }}>⬅</p>
           <div className={"button button-right"} onClick={(e) => 
           {
+              // scroll right on button click
               const scrollContainer = document.querySelector(".image-wrapper");
               scrollContainer.scrollLeft += 250;
           }}>➡</div>
--- src/components/topic-image-container.js
+++ src/components/topic-image-container.js
@@ -1,8 +1,16 @@
+import {useEffect} from 'react';
+
 export function TopicImageContainer({photosSetOne, photosSetTwo}) {
+
+    useEffect(() => {
+        window.scrollTo(0, 0)
+    }, []);
+
     return (
         <div className={"image-container"}>
             <div className={"image-wrapper"} onWheel = {(e) => 
-            { 
+            {   
+                // scroll left and right on mouse wheel scroll
                 const scrollContainer = document.querySelector(".image-wrapper");
                 scrollContainer.scrollLeft -= e.deltaY;
             }}>
@@ -15,12 +23,14 @@ export function TopicImageContainer({photosSetOne, photosSetTwo}) {
                 <img className={"image-item"} src={image.urls.regular} key={image.id}/>
             ))}
             <div className={"button button-left"} onClick={(e) => 
-            {
+            {   
+                // scroll left on button click
                 const scrollContainer = document.querySelector(".image-wrapper");
                 scrollContainer.scrollLeft -= 250;
             }}>⬅</div>
             <div className={"button button-right"} onClick={(e) => 
             {
+                // scroll right on button click
                 const scrollContainer = document.querySelector(".image-wrapper");
                 scrollContainer.scrollLeft += 250;
             }}>➡</div>
